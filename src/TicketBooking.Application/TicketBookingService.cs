484d5d0959d46357229aa24e56adedea43266099
Lloyd Atkinson <lloydjatkinson@gmail.com>
2023-01-26 21:54:41 +0000

    feature: setup empty project with passing test for pipeline

---
+++ src/TicketBooking.Application/TicketBookingService.cs
@@ -0,0 +1,7 @@
+﻿namespace TicketBooking.Application
+{
+    public class TicketBookingService
+    {
+
+    }
+}
---
+++ test/TicketBooking.Test/MathsTest.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace TicketBooking.Test
+{
+    public class MathsTest
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        public void AddingPositiveNumbersReturnsPositiveNumber(int first, int second)
+        {
+            var result = first + second;
+
+            result.ShouldBePositive();
+        }
+    }
+}
